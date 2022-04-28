66d7b940a8192371cd20761387a662154074e5b2
Lee Pritchard <lee_pritchard@rocketmail.com>
2022-04-28 01:52:42 +0100

    v0.072
    Added leadboard page buttons when there are more than 10 entries for the guild
    **should** work but it's 2am and I don't have more than 10 entries on my test server so who knows?
    
    Previous page button should disabled itself when on page 1 and enable for any page after that

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('leaderboard')
 		.setDescription('Show the leaderboard'),
 	async execute(interaction) {
-		const { MessageEmbed } = require('discord.js');
+		const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 		const { Users } = require('../currencyShopDB/csDBObjects.js');
 		const { Guilds } = require('../guildDB/guilddbObjects');
 		const currentGuild = await Guilds.findOne({ where: { guild_id: interaction.guildId } });
@@ -25,9 +25,11 @@ module.exports = {
 			return interaction.reply({ content: `You need to use this in the designated shop channel: <#${currentGuild.shop_channel_id}>`, ephemeral: true });
 		}
 		else {
+			let pageNumber = 1;
 			const leaderboardEmbed = new MessageEmbed()
 				.setColor('RED')
-				.setTitle(`Leaderboard of "${interaction.guild.name}`);
+				.setTitle(`Leaderboard of "${interaction.guild.name}"`)
+				.setFooter({ text: `Page ${pageNumber}` });
 
 			const guildUsers = await Users.findAll({ where: { guild_id: interaction.guildId }, order: [['balance', 'DESC']] });
 
@@ -35,7 +37,50 @@ module.exports = {
 
 			lbPage[0].map((i, index) => leaderboardEmbed.addField(index + 1 + ' - ' + i.username, currentGuild.shop_currency + ' ' + i.balance));
 
-			return interaction.reply({ embeds: [leaderboardEmbed] });
+			const buttonRow = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId('previous-page')
+						.setLabel('Previous Page')
+						.setStyle('DANGER')
+						.setDisabled(true),
+				)
+				.addComponents(
+					new MessageButton()
+						.setCustomId('next-page')
+						.setLabel('Next Page')
+						.setStyle('PRIMARY'),
+				);
+
+			if (lbPage[1] !== undefined) {
+				interaction.reply({ embeds: [leaderboardEmbed], components: [buttonRow] });
+			}
+			else {
+				interaction.reply({ embeds: [leaderboardEmbed] });
+			}
+
+			const buttonCollector = interaction.channel.createMessageComponentCollector({ componentType: 'BUTTON', time: 60000 });
+
+			buttonCollector.on('collect', async e => {
+				if (e.user.id === interaction.user.id) {
+					if (e.customId === 'next-page' && lbPage[pageNumber] !== undefined && leaderboardEmbed.footer.text === 'Page 1') {
+						lbPage[pageNumber].map((i, index) => leaderboardEmbed.addField(index + 1 + ' - ' + i.username, currentGuild.shop_currency + ' ' + i.balance));
+						leaderboardEmbed.setFooter({ text: `Page ${pageNumber}` });
+						pageNumber++;
+						buttonRow.customId('previous-page').setDisabled(false).setStyle('PRIMARY');
+						await e.editReply({ embeds: [leaderboardEmbed], components: [buttonRow] });
+					}
+					else if (e.customId === 'previous-page' && lbPage[pageNumber - 1] !== undefined) {
+						lbPage[pageNumber - 1].map((i, index) => leaderboardEmbed.addField(index + 1 + ' - ' + i.username, currentGuild.shop_currency + ' ' + i.balance));
+						leaderboardEmbed.setFooter({ text: `Page ${pageNumber}` });
+						pageNumber--;
+						if (pageNumber === 1) {
+							buttonRow.customId('previous-page').setDisabled(true).setStyle('DANGER');
+						}
+					}
+				}
+			});
+			return;
 		}
 	},
 };
