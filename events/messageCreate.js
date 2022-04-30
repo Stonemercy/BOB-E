f3d412e0d88afb689ae2fd30bf0ee565d4bed8a5
Lee Pritchard <lee_pritchard@rocketmail.com>
2022-04-30 01:37:27 +0100

    v0.074
    Potential fix to currency updating throwing an error

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,7 +4,6 @@ module.exports = {
 		const { Users } = require('../currencyShopDB/csDBObjects.js');
 		const { Guilds } = require('../guildDB/guilddbObjects.js');
 		const currentGuild = await Guilds.findOne({ where: { guild_id: message.guild.id } });
-		const currentGuildUser = await Users.findOne({ where: { user_id: message.author.id, guild_id: message.guildId } });
 
 		if (currentGuild !== null && message.author.id === currentGuild.webhook_id && message.channel.id === currentGuild.webhook_channel_id) {
 			const steamIDCheck = /\d{17}/g;
@@ -16,9 +15,14 @@ module.exports = {
 			if (steamID && time && userSteamDBCheck) {
 				const [ timeHours, timeMins ] = time.toString().split(':');
 				const totalMins = (+timeMins) + (+timeHours) * 60;
-				await currentGuildUser.update({ balance: currentGuildUser.balance += totalMins });
-				console.log(`User ${steamID} played for ${totalMins} mins`);
-				return;
+				if (userSteamDBCheck) {
+					await userSteamDBCheck.update({ balance: userSteamDBCheck.balance += totalMins });
+				}
+				else {
+					return console.log(`User ${steamID} has left their server but hasn't registered their Discord account`);
+				}
+				return console.log(`User ${steamID} played for ${totalMins} mins`);
+
 			}
 			else if (message.content.match(/(left server)/g)) {
 				return console.log(`User ${steamID} has left their server but hasn't registered their Discord account`);
@@ -26,13 +30,8 @@ module.exports = {
 
 		}
 		else if (!message.author.bot) {
+			const currentGuildUser = await Users.findOne({ where: { user_id: message.author.id, guild_id: message.guild.id } });
 			if (currentGuildUser) {
-				/* const NinetyNinePercent = Math.floor(Math.random() * 101);
-				console.log(NinetyNinePercent);
-				if (NinetyNinePercent >= 99) { */
-				await currentGuildUser.update({ balance: currentGuildUser.balance += Number(1) });
-				// message.channel.send(`${message.author.username} just got the random 1% bonus of 1000💰`);
-				// }
 				return;
 			}
 			else {
